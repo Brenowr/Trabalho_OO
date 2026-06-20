d829d301c75b6029f61a9e20a36eea27b6e4c827
sakai-ano <gabrielsakai2006@gmail.com>
2026-06-20 11:58:13 -0300

    criacao do codigo de cada pedido

--- byte_brew/src/br/edu/cafeteria/modelo/Pedido.java
+++ byte_brew/src/br/edu/cafeteria/modelo/Pedido.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 public class Pedido {
 	private ArrayList<ItemPedido> itens;
 	private Cliente cliente;
+	private static int codigoPedido = 0;
 	
 	public Pedido(Cliente cliente) {
 		this.cliente = cliente;
 		this.itens = new ArrayList<>();
+		this.codigoPedido++;
 		
 	}
 
