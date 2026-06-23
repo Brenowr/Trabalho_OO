a820358d0d1f70661d703635ab3441de70b5644a
Sarah Cristiny Marques de Sousa <scmarquesdesousa@gmail.com>
2026-06-23 13:33:52 -0300

    adiçao do metodo para remover xp

--- byte_brew/src/br/edu/cafeteria/modelo/Cliente.java
+++ byte_brew/src/br/edu/cafeteria/modelo/Cliente.java
@@ -2,25 +2,38 @@ package br.edu.cafeteria.modelo;
 
 public abstract class Cliente {
 	private String nomeCliente,cpf;
-	private int experiencia;
+	private int xp;
 	
 	public Cliente(String nomeCliente, String cpf) {
 		this.nomeCliente = nomeCliente;
 		this.cpf = cpf;
+		this.xp = 0;
 	}
 	
 	public void adicionarXP(int adicionar)
 	{
-		experiencia += adicionar;
+		xp += adicionar;
 	}
+	
+	public void removerXp(int adicionar) {
+		 xp -= adicionar;
+	 }
 			
 	public abstract void calcularXP(double valorTotal);
 	
-	 public int getExperiencia() {
-	    return experiencia;
+	 public int getXp() {
+	    return xp;
 	}
-
+	 
 	 public String getNome() {
 		return nomeCliente;
+	 }
+	 
+	 public String getCpf() {
+		 return cpf;
+	 }
+
+	 public void setNome(String novoNome) {
+		 nomeCliente = novoNome;
 	 }
 }
